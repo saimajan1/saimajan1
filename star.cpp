8fd7405edf2dbc0197dad3bcee5fd93d754d6410
saima jan <97116326+saimajan1@users.noreply.github.com>
2022-01-19 23:40:31 +0530

    Create star.cpp

---
+++ star.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+ int main()
+ {
+     int a,b;
+     {
+     for(a=8;a<=30;a++)
+     {
+     for(b=1;b<a;b++)
+     
+     cout<<"*****************"<<endl;
+    return 0;
+     }
+     }
+ }
